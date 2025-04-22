5326e596e4eb2ef2ccc2500699783ef442ddc79f
Lugo,Michael Angelo <lugomichael988@gmail.com>
2025-04-22 15:34:22 +0800

    add ProductItem class

--- SweetBoxInventorySystem/SweetBoxInventorySystem/Models/Product.cs
+++ SweetBoxInventorySystem/SweetBoxInventorySystem/Models/Product.cs
@@ -13,6 +13,7 @@ namespace SweetBoxInventorySystem.Models
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public decimal ProductPrice { get; set; }
+      
 
         public Product(int productId, string productName, decimal productPrice)
         {
