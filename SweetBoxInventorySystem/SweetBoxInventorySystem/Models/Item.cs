a3aa3e86350f6cddd6188eb4a2e4a7624dba009d
Lugo,Michael Angelo <lugomichael988@gmail.com>
2025-04-23 19:43:39 +0800

    add method of a class item

--- SweetBoxInventorySystem/SweetBoxInventorySystem/Models/Item.cs
+++ SweetBoxInventorySystem/SweetBoxInventorySystem/Models/Item.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.Data.Utils;
+using DevExpress.Skins;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace SweetBoxInventorySystem.Models
 {
-    public class Item
+    public class Item : Product
     {
         public string SyrupPumps { get; set; }
 
@@ -17,7 +18,7 @@ namespace SweetBoxInventorySystem.Models
 
         public string Stirrers { get; set; }
 
-        public string straws { get; set; }
+        public string Straws { get; set; }
 
         public string Napkins { get; set; }
 
@@ -27,7 +28,26 @@ namespace SweetBoxInventorySystem.Models
 
         public string TogoBags { get; set; }
 
-
+        public Item(int productId, string productName, decimal productPrice, int productQuantity, 
+            DateTime expiryDate, string productType, string syrupPumps , string cups, string lidsAndSleeves,
+            string stirrers, string straws, string napkins, string sugarPackets, string creamers, string togoBags) :
+           base(productId, productName, productPrice, productQuantity, expiryDate)
+        {
+            SyrupPumps = syrupPumps;
+            Cups = cups;
+            LidsAndSleeves = lidsAndSleeves;
+            Stirrers = stirrers;
+            Straws = straws;
+            Napkins = napkins;
+            SugarPackets = sugarPackets;
+            Creamers = creamers;
+            TogoBags = togoBags;
+            
+        }
+        public override string GetProductInfo()
+        {
+            return $"Item: {SyrupPumps}, {Cups} ,{LidsAndSleeves}, {Stirrers} ,{Straws}, {Napkins} ,{SugarPackets}, {Creamers} ,{TogoBags}";
+        }
 
     }
 }
